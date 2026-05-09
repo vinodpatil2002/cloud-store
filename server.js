b61b586b98edea27476a9bd3a2d3b3397cc27285
vinodpatil2002 <vinodnspatil@gmail.com>
2026-05-09 17:50:30 +0530

    Add CORS support to server and update package dependencies

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
 const authRoutes = require("./routes/auth.route.js");
 const fileRoutes = require("./routes/file.route.js");
 
 const app = express();
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "*",
+    methods: ["GET", "POST", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
 app.use(express.json());
 
 mongoose
